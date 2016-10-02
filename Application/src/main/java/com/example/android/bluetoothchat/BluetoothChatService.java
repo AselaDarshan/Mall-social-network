518087928b7f3f71b5071c151935e3a9f2163b41
AselaDarshan <aseladarshan@gmail.com>
2016-10-02 23:03:45 +0530

    Can Connect to 3 devices

--- Application/src/main/java/com/example/android/bluetoothchat/BluetoothChatService.java
+++ Application/src/main/java/com/example/android/bluetoothchat/BluetoothChatService.java
@@ -588,7 +588,7 @@ public class BluetoothChatService {
                 Log.d(TAG,"Sending message to discovered devices");
                 while(!BluetoothChatService.this.isFinished()){
                     try {
-                        Thread.sleep(100);
+                        Thread.sleep(1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
