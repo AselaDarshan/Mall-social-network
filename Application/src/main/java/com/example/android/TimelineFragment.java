9a90e1b5ec5f953841014e62f1bae1f4dbf392c4
Dinindu-Nissanka <dmmnissanka@gmail.com>
2016-10-03 02:20:05 +0530

    Created fragment view

--- Application/src/main/java/com/example/android/TimelineFragment.java
+++ Application/src/main/java/com/example/android/TimelineFragment.java
@@ -1,60 +1,93 @@
 package com.example.android;
 
-import android.app.Activity;
-import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Toast;
+import android.view.ViewGroup;
 
+import com.example.android.MsgAdapter;
 import com.example.android.bluetoothchat.Msg;
 import com.example.android.bluetoothchat.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Timeline extends Activity {
+public class TimelineFragment extends Fragment {
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private List<Msg> itemList = new ArrayList<>();
+    private boolean timeline = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timeline);
-        mRecyclerView = (RecyclerView) findViewById(R.id.msg_recycler_view);
-
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
-
-        // use a linear layout manager
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        // specify an adapter (see also next example)
-        mAdapter = new MsgAdapter(itemList, Timeline.this);
-        mRecyclerView.setAdapter(mAdapter);
-
         /*FloatingActionButton FAB = (FloatingActionButton) findViewById(R.id.fab);
         FAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Timeline.this, NewMessage.class);
+                Intent intent = new Intent(TimelineFragment.this, NewMessage.class);
                 startActivity(intent);
             }
         });*/
+    }
 
-        setUpData();
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_timeline, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.msg_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MsgAdapter(itemList, getActivity());
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onStart() {
+        Log.d("Fragment Start","Start Fragment");
+        super.onStart();
+        if(!timeline)
+            setUpData();
+    }
+
+
+    @Override
+    public void onDestroy() {
+        Log.d("Freagment Destroyed","Destroy Fragment");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onResume() {
+        Log.d("Fragment On Resume", "Resume Fragment");
+        super.onResume();
+        if(!timeline)
+            setUpData();
     }
 
     private void setUpData() {
+        timeline = true;
         /*String uid, Timestamp tstamp, int type, String inReplyToMessageID, String text, int rank, int noOfRankers, String image*/
         Msg item;
 
--- Application/src/main/java/com/example/android/bluetoothchat/BluetoothChatFragment.java
+++ Application/src/main/java/com/example/android/bluetoothchat/BluetoothChatFragment.java
@@ -408,18 +408,6 @@ public class BluetoothChatFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.secure_connect_scan: {
-//                // Launch the DeviceListActivity to see devices and do scan
-//                Intent serverIntent = new Intent(getActivity(), DeviceListActivity.class);
-//                startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_SECURE);
-                return true;
-            }
-            case R.id.insecure_connect_scan: {
-//                // Launch the DeviceListActivity to see devices and do scan
-//                Intent serverIntent = new Intent(getActivity(), DeviceListActivity.class);
-//                startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_INSECURE);
-                return true;
-            }
             case R.id.discoverable: {
                 // Ensure this device is discoverable by others
                 ensureDiscoverable();
